f6de4b958e577f9a88ef10e5a2016e5f794b4b52
SUP3RBOB <98238342+SUP3RBOB@users.noreply.github.com>
2023-09-21 16:26:18 -0400

    Fixed the subtract method so that it actually does its job

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,7 +3,7 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
-
+void subtract();
 
 void main() {
 
@@ -16,10 +16,9 @@ void main() {
 	printf("Enter operation number: ");
 	scanf_s("%1o", &inputNum);
 
-	switch (inputNum)
-	{
-	case 1:
-		add();
+	switch (inputNum) {
+		case 1: add(); break;
+		case 2: subtract(); break;
 	}
 
 }
@@ -44,4 +43,14 @@ void add() {
 	scanf_s("%lf", &num2);
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
+}
+
+void subtract() {
+	double num1, num2, result;
+	printf("Enter the first value:");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value:");
+	scanf_s("%lf", &num2);
+	result = num1 - num2;
+	printf("%lf - %lf = %lf\n", num1, num2, result);
 }
